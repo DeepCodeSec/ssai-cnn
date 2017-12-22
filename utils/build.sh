d47f804845b1c923ebb2a9d2cda3c82f20feb47a
Jonathan Racicot <jonathan.racicot@rmc.ca>
2017-12-22 00:56:55 -0500

    Build utils without Anaconda.

--- utils/build.sh
+++ utils/build.sh
@@ -1,8 +1,8 @@
-PYTHON_DIR=$HOME/.pyenv/versions/anaconda3-2.4.0
+PYTHON_DIR=/usr/bin
 cmake \
--DPYTHON_LIBRARY=$PYTHON_DIR/lib/libpython3.5m.so \
--DPYTHON_INCLUDE_DIR=$PYTHON_DIR/include/python3.5m \
--DPYTHON_INCLUDE_DIR2=$PYTHON_DIR/include \
+-DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.5m.so \
+-DPYTHON_INCLUDE_DIR=/usr/include/python3.5m \
+-DPYTHON_INCLUDE_DIR2=/usr/include \
 -DBoost_INCLUDE_DIR=/usr/local/include \
 -DBoost_NumPy_INCLUDE_DIR=/usr/local/include \
 -DBoost_NumPy_LIBRARY_DIR=/usr/local/lib \
