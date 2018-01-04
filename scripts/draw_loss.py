644c2c79fe0b302e7e3891ec0552ae0a4f7c0f92
Jonathan Racicot <jonathan.racicot@rmc.ca>
2018-01-04 01:36:17 -0500

    Fix RE for loss by considering NaN.

--- scripts/draw_loss.py
+++ scripts/draw_loss.py
@@ -24,10 +24,12 @@ def draw_loss(logfile, outfile):
             continue
         epoch = int(re.search('epoch:([0-9]+)', line).groups()[0])
         if 'iter' not in line and 'train loss' in line:
-            tr_l = float(re.search('loss:([0-9\.e-]+)', line).groups()[0])
+            tr_s = re.search('loss:([0-9\.e-|nan]+)', line)
+            tr_l = float(tr_s.groups()[0])
             train_epoch_loss.append([epoch, tr_l])
         if 'iter' not in line and 'validate loss' in line:
-            va_l = float(re.search('loss:([0-9\.e-]+)', line).groups()[0])
+            va_s = re.search('loss:([0-9\.e-|nan]+)', line)
+            va_l = float(va_s.groups()[0])
             valid_epoch_loss.append([epoch, va_l])
 
     train_epoch_loss = np.asarray(train_epoch_loss)
